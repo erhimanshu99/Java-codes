ef4b37f2635a0d060d6556c5e918c5731853edcb
Himanshu Kumar <erhimanshu799@gmail.com>
2024-08-31 18:45:49 +0530

    Changes

---
+++ Array/Array1.java
@@ -0,0 +1,9 @@
+package Array;
+class Array1
+{
+public static void main(String[] args) 
+{
+    int[] x= new int[3];
+    System.out.println(x);
+}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Java-codes
-My java practice codes will be here
